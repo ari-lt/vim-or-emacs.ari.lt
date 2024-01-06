8f1da22ecbfe69d5f338ea3d8a0806e82313831a
Ari Archer <ari.web.xyz@gmail.com>
2024-01-06 08:20:01 +0200

    add kill to run.sh
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,7 @@ set -u
 
 main() {
     cd src
+    kill -9 $(pgrep python3) || true
     python3 -m pip install gunicorn
     python3 -m gunicorn -b 127.0.0.1:8001 -w "$(nproc --all)" main:app &
     disown
