5522c7146074a16fc0fcb8915dbb533d3f22a041
Ari Archer <ari.web.xyz@gmail.com>
2024-01-06 08:21:25 +0200

    make it use 1 worker ( as its sqlite )
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ main() {
     cd src
     kill -9 $(pgrep python3) || true
     python3 -m pip install gunicorn
-    python3 -m gunicorn -b 127.0.0.1:8001 -w "$(nproc --all)" main:app &
+    python3 -m gunicorn -b 127.0.0.1:8001 -w 1 main:app &
     disown
 }
 
